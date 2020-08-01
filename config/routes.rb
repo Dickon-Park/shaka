3961d63de62cb7c65427ce137bfec8a289f87bdc
raissa borba <raissavborba@gmail.com>
2020-08-01 13:00:56 +0200

    favourites routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :spots do
+    resources :favourites, only: [ :new, :create ]
     resources :reviews, only: [ :new, :create ]
   end
+  resources :favourites, only: [ :destroy ]
 end
