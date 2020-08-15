9ed43daf9ed97df89a3524fc1e52a242f3e1173d
raissa borba <raissavborba@gmail.com>
2020-08-15 14:12:10 +0200

    user references added to spots, btn danger added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_182416) do
+ActiveRecord::Schema.define(version: 2020_08_15_120327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,8 @@ ActiveRecord::Schema.define(version: 2020_08_11_182416) do
     t.string "address"
     t.float "longitude"
     t.float "latitude"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_spots_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
