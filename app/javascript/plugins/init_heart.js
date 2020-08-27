ce60c3d5ec3910f718626767985b9e5caff0a49d
raissa borba <raissavborba@gmail.com>
2020-08-27 19:25:59 +0200

    avatar reviews fixed

--- app/javascript/plugins/init_heart.js
+++ app/javascript/plugins/init_heart.js
@@ -1,7 +1,6 @@
 const initHeart = () => {
     const heartLinks = document.querySelector(".heart-red > a");
     heartLinks.addEventListener("click", (event) => {
-        console.log(event);
         event.preventDefault();
     });
 };
